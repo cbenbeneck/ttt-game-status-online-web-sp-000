bd4189dd843918be0a5eb9977643cf0647ca2ce8
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-09 03:31:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,9 +29,9 @@ WIN_COMBINATIONS = [
       if ((position_1 == "X" && position_2 == "X" && position_3 == "X")  || (position_1 == "O" && position_2 == "O" && position_3 == "O")) 
         return win_combination
       else
-       return false 
       end 
     end 
+       return false 
   end
   
   def full?(board)
