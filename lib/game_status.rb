be66d646cd963f1fd0ae5aa1730ae80a20cb18a8
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-09 03:25:50 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,17 +26,13 @@ WIN_COMBINATIONS = [
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
       
-      if position_1 == "X" && position_2 == "X" && position_3 == "X" 
-         return win_combination
-        
-      else if position_1 == "O" && position_2 == "O" && position_3 == "O" 
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"  || position_1 == "O" && position_2 == "O" && position_3 == "O" 
         return win_combination
       else
        return false 
       end 
     end 
   end
-end 
   
   def full?(board)
     if board.all? {|index| index == "X" || index == "O"}  
